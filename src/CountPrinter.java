a417608db6ea6b5efa1ce96de52dc35e0643b31c
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-09-30 14:21:12 +0200

    implemented classes DatePrinter and CountPrinter

--- src/CountPrinter.java
+++ src/CountPrinter.java
@@ -1,2 +1,13 @@
-public class CountPrinter {
+/**
+ * @author Paul Duschek
+ * @version 1.0, 30.9.21
+ */
+
+public class CountPrinter implements Runnable{
+    @Override
+    public void run() {
+        for(int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+    }
 }
--- src/DatePrinter.java
+++ src/DatePrinter.java
@@ -1,2 +1,17 @@
-public class DatePrinter {
+/**
+ * @author Paul Duschek
+ * @version 1.0, 30.9.21
+ */
+
+import java.util.Date;
+
+public class DatePrinter implements Runnable{
+    @Override
+    public void run() {
+        Date now = new Date();
+        now.getTime();
+        for(int i = 0; i < 20; i++) {
+            System.out.println(now);
+        }
+    }
 }
