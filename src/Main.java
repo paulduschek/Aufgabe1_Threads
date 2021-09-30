664c346184c7b12e5a7eb0888fff76b33aec1ded
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-09-30 14:23:04 +0200

    implemented class Main

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        
+        DatePrinter dp = new DatePrinter();
+        Thread th1 = new Thread(dp);
+
+        CountPrinter cp = new CountPrinter();
+        Thread th2 = new Thread(cp);
+
+        th1.start();
+        th2.start();
     }
 }
